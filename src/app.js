4d13eb18772e89c522144d2b8846e40343d96bf9
선재영 <wodud1892@naver.com>
2024-06-10 15:14:03 +0900

    setting

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import { createServer } from 'http';
+import initSocket from './init/socket.js';
+
+const app = express();
+const server = createServer(app);
+
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+initSocket(server); // 소켓 추가
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World</h1>');
+});
+
+server.listen(PORT, async () => {
+  console.log(`Server is running on port ${PORT}`);
+});
