006b8fdf0a5a70ef72894981c5c0ce0e278e3282
선재영 <wodud1892@naver.com>
2024-06-10 16:26:48 +0900

    assetsUpload

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { createServer } from 'http';
 import initSocket from './init/socket.js';
+import { loadGameAssets } from './init/assets.js';
 
 const app = express();
 const server = createServer(app);
@@ -9,7 +10,7 @@ const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-initSocket(server); // 소켓 추가
+initSocket(server);
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World</h1>');
@@ -17,4 +18,12 @@ app.get('/', (req, res) => {
 
 server.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
+
+  try {
+    const assets = await loadGameAssets();
+    console.log(assets);
+    console.log('Assets loaded successfully');
+  } catch (error) {
+    console.error('Failed to load game assets:', error);
+  }
 });
