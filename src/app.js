d2c4077d84d7d50ff9100b8bbc24e4928e73acf8
선재영 <wodud1892@naver.com>
2024-06-13 18:51:56 +0900

    cors

--- src/app.js
+++ src/app.js
@@ -2,11 +2,20 @@ import express from 'express';
 import { createServer } from 'http';
 import initSocket from './init/socket.js';
 import { loadGameAssets } from './init/assets.js';
+import cors from 'cors'; 
+
 
 const app = express();
 const server = createServer(app);
 
 const PORT = 3000;
+
+// CORS 설정 추가
+app.use(cors({
+  origin: 'http://43.201.83.246:3000', // 허용할 프론트엔드 주소
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // 허용할 HTTP 메소드
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'] // 허용할 헤더
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
